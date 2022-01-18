d0d1f2309d97f68df0f749e11d61eed5db531a7d
sakamuuy <sakamuu.y@gmail.com>
2022-01-18 23:39:51 +0900

    Add emotion, router

---
+++ src/components/atoms/Button/MainButton/index.tsx
@@ -0,0 +1,20 @@
+import { css } from '@emotion/react'
+
+export function MainButton() {
+  const color = 'white'
+  return (
+    <div
+    css={css`
+      padding: 32px;
+      background-color: hotpink;
+      font-size: 24px;
+      border-radius: 4px;
+      &:hover {
+        color: ${color};
+      }
+    `}
+  >
+    Hover to change color.
+  </div>
+  )
+}
